5ea87caf6728c397f92e308905a158237b0df2f4
naubergois <naubergois@gmail.com>
2016-09-03 13:16:29 -0300

    bug correction

--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,34 @@
-#Merges the training and the test sets to create one data set.
+library(plyr)
+require(tcltk)
 
-xtrain <- read.table("X_train.txt")
-xtest <- read.table("X_test.txt")
+#Merges the training and the test sets to create one data set.
+message("Choose X_train")
+xtrainfile<-file.choose()
+xtrain <- read.table(xtrainfile)
+message("Choose X_test")
+xtestfile<-file.choose()
+xtest <- read.table(xtestfile)
 X <- rbind(xtrain, xtest)
 
-subject_train <- read.table("subject_train.txt")
-subject_test <- read.table("subject_test.txt")
+message("Choose Subject train")
+subjecttrainfile<-file.choose()
+subject_train <- read.table(subjecttrainfile)
+message("Choose Subject test")
+subjecttestfile<-file.choose()
+subject_test <- read.table(subjecttestfile)
 S <- rbind(subject_train, subject_test)
 
-ytrain <- read.table("y_train.txt")
-ytest <- read.table("y_test.txt")
+message("Choose Y train")
+ytrainfile<-file.choose()
+ytrain <- read.table(ytrainfile)
+message("Choose Y test")
+ytestfile<-file.choose()
+ytest <- read.table(ytestfile)
 Y <- rbind(ytrain, ytest)
 
-features <- read.table("features.txt")
+message("Choose features file")
+featuresfile<-file.choose()
+features <- read.table(features)
 featuresvector<-as.character(features[,2])
 #Extracts only the measurements on the mean and standard deviation for each measurement.
 
@@ -37,9 +53,25 @@ result <- cbind(S, Y, X)
 names(result)[1:2]<-c("subject", "activity")
 names(result)[3:81]<-as.character(features$V2[c(featureindex$V1)])
 
+
+#start seperating out featName column to seperate columns
+names(result) <- gsub('^t', 'time', names(result))
+names(result) <- gsub('^f', 'frequency', names(result))
+names(result) <- gsub('Acc', 'Accelerometer', names(result))
+names(result) <- gsub('Gyro','Gyroscope', names(result))
+names(result) <- gsub('mean[(][)]','Mean',names(result))
+names(result) <- gsub('std[(][)]','Std',names(result))
+names(result) <- gsub('-','',names(result))
+
+
+
+
 #From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-labels<-colnames(result)[-c(1,2)]
-data <- lapply(X =labels, FUN=function(x) tapply(result[,x], list(result$activity, result$subject), mean))
 
 
-write.table(data, "merged_data.txt")
+tidy<-aggregate(. ~subject + activity, result, mean)
+tidy<-tidy[order(tidy$subject,tidy$activity),]
+write.table(tidy, file = "tidy.txt",row.name=FALSE)
+
+
+
